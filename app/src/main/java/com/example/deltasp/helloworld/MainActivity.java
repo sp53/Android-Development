371d295e6f4c4b9a43200001030f4f5ea2ff926e
Sparsh Bansal <sparshbansal53@gmail.com>
2018-10-21 17:25:41 +0530

    uploading

---
+++ app/src/main/java/com/example/deltasp/helloworld/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.deltasp.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textView2);
+        // cooment
+    }
+
+    public void onButtonTap(View v) {
+        Toast obj = Toast.makeText(getApplicationContext(), "Sup Bitches !! ", Toast.LENGTH_LONG);
+        obj.show();
+
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
